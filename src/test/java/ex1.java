4ea5478947ca9eb810de20fb501a6e400bcd30a7
tatianajmd <tatianajmd@gmail.com>
2025-02-21 17:31:01 -0500

    2nd commit

---
+++ src/test/java/ex1.java
@@ -0,0 +1,11 @@
+public class ex1 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+    }
+}
